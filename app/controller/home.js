fc0d3c70a77e848c8ac5f9336b292cbddcd9db9c
baixiaojian <15101661380@163.com>
2018-11-14 19:33:33 +0800

    🥄 feat: default response

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+    this.ctx.body = {
+      code: 0,
+      message: '成功',
+      data: null,
+    };
   }
 }
 
