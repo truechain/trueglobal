883b1a80704db98940106becd5e0d67163733c69
baixiaojian <15101661380@163.com>
2018-11-21 10:12:45 +0800

    update topic title

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
 .el-collapse-item__wrap{
   border: none;
   border-radius:0 0 10px 10px;
-  background:rgba(242,245,250,1);
+  background:rgba(242,245,250,1) !important;
   padding: 10px;
 }
 </style>
