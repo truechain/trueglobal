b66e3ff64691258088b4381e48de789b1095c5d7
baixiaojian <15101661380@163.com>
2018-11-22 18:30:46 +0800

    feat: auto deploy script

---
+++ webhook.js
@@ -0,0 +1,52 @@
+'use strict';
+const http = require('http');
+const axios = require('axios');
+const createHandler = require('github-webhook-handler');
+const { spawn } = require('child_process');
+const handler = createHandler({
+  path: '/webhook',
+  secret: 'true_global_1380',
+});
+
+const runCommand = (cmd, args, callback) => {
+  const child = spawn(cmd, args);
+  let response = '';
+  child.stdout.on('data', buffer => {
+    response += buffer.toString();
+  });
+  child.stdout.on('end', () => callback(response));
+};
+
+http.createServer(function(req, res) {
+  handler(req, res, function(err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+}).listen(7775);
+
+handler.on('error', function(err) {
+  console.error('Error:', err.message);
+});
+
+handler.on('push', function(event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+  if (event.payload.ref === 'refs/heads/master') {
+    console.log('Start run command on Master');
+    runCommand('sh', [ './auto_deploy.sh' ], txt => {
+      console.log(txt);
+    });
+  } else if (event.payload.ref === 'refs/heads/server') {
+    console.log('Start run command on Server');
+    // axios.get('http://47.75.198.92:7770/')
+    //   .then(response => {
+    //     console.log(response.data, 'server');
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+  } else {
+    console.log('Not the master branch, will not trigger');
+  }
+});
