2c263466c2d67f7342561ecf5cbb300dd45a7798
see91 <loverrenmin@qq.com>
2018-11-22 10:39:32 +0800

    add

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -207,7 +207,7 @@ export default {
       var j = i + 1
       this.mechanism.push(require('../assets/cooperativeInstitution/' + j + '.png'))
     }
-    for (var x = 0; x < 3; x++) {
+    for (var x = 0; x < 4; x++) {
       var p = x + 1
       this.co_hosting.push(require('../assets/co_hosting/' + p + '.png'))
     }
