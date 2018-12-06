893f5662323a9a58b613cd5bed2fff329e99c7ac
baixiaojian <15101661380@163.com>
2018-12-06 17:25:21 +0800

    add news page

---
+++ src/pages/Detail.vue
@@ -0,0 +1,142 @@
+<template lang="pug">
+  .news-detail-container
+    .news-detail
+      .news-detail-title {{ detail.title }}
+      .news-detail-timestamp(v-text="getTime(+detail.create_time, true)")
+      .news-detail-content(v-html="detail.content")
+</template>
+
+<script>
+import { apiArticleDetail } from '@/api'
+import { getTime } from '@/util'
+export default {
+  head () {
+    return {
+      title: this.$t('news.head.title'),
+      meta: [
+        { hid: 'news-keyword', name: 'keyword', content: this.$t('news.head.keyword') },
+        { hid: 'news-description', name: 'description', content: this.$t('news.head.description') }
+      ]
+    }
+  },
+  data () {
+    return {
+      detail: {},
+      spinShow: true
+    }
+  },
+  methods: {
+    getTime
+  },
+  mounted () {
+    apiArticleDetail({
+      id: this.$route.params.id
+    }).then(res => {
+      this.detail = res.data.data[0] || {}
+    })
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~@/assets/stylus/mixin.styl'
+  /* 覆盖分页样式 */
+.news-detail
+  display flex
+  flex-direction column
+  margin 0 auto
+  width 62%
+  // background red
+  padding-top 150px
+  padding-bottom 25px
+  position relative
+.news-detail-title
+  color $font-dark
+  text-align center
+  font-size 20px
+  width 100%
+  font-weight bold
+  position relative
+  padding-bottom 30px
+.news-detail-content
+  // background red
+  margin-top 15px
+  line-height 25px
+  font-size 14px
+  p
+    text-indent 2em
+  span, p
+    color $font-dark
+
+.news-detail-timestamp
+  text-align right
+  font-size 12px
+  color #456C99
+.news-detail-container
+  h1
+    font-size 2em
+    margin 0.67em 0
+  h2
+    font-size 1.5em
+    margin 0.75em 0
+  h3
+    font-size 1.17em
+    margin 0.83em 0
+  h4, p, blockquote, ul, fieldset, form, ol, dl, menu
+    margin 1.12em 0
+  h5
+    font-size .83em
+    margin 1.5em 0
+  h6
+    font-size .75em
+    margin 1.67em 0
+  h1, h2, h3, h4, h5, h6, b, strong
+    font-weight bolder
+  table
+    display table
+  tr
+    display table-row
+  thead
+    display table-header-group
+  tbody
+    display table-row-group
+  tfoot
+    display table-footer-group
+  td, th
+    display table-cell
+  caption
+    display table-caption
+  th
+    font-weight bolder
+    text-align center
+  caption
+    text-align center
+  table
+    border-spacing 2px
+  thead, tbody, tfoot
+    vertical-align middle
+  td, th
+    vertical-align inherit
+  blockquote
+    margin-left 40px
+    margin-right 40px
+  pre, tt, code, kbd, samp
+    font-family monospace
+  pre
+    white-space pre
+  big
+    font-size 1.17em
+  small, sub, sup
+    font-size 0.83em
+  sub
+    vertical-align sub
+  sup
+    vertical-align super
+  s, strike, del
+    text-decoration line-through
+  hr
+    border 1px inset
+  img
+    width auto
+    height auto
+</style>
---
+++ src/pages/News.vue
@@ -0,0 +1,179 @@
+
+<template lang="pug">
+  .news-container
+    .news-body
+      //- .page-title {{$t('news.title')}}
+      .news-body-table
+        .news-body-table-btn
+          div(
+            class="news-body-table-btn-avtive"
+          ) {{$t('news.title')}}
+          // div(
+          //   :class="{ 'news-body-table-btn-avtive': nodeType === 1 }",
+          //   @click="toggleNode(1)"
+          // ) {{$t('news.technical')}}
+          // div(
+          //   :class="{ 'news-body-table-btn-avtive': nodeType === 2 }",
+          //   @click="toggleNode(2)"
+          //   ) {{$t('news.biz')}}
+        .news-body-table-body
+          ul
+            template(v-if="list.length")
+              router-link(
+                tag="li",
+                v-for="(item, index) in list",
+                :key="index",
+                :to="`detail/${item.id}`",
+              )
+                .news-body-table-body-date {{ getTime(+item.create_time, true) }}
+                .news-body-table-body-title.ellipsis  {{item.title}}
+            template(v-else)
+              li
+                .news-body-table-body-date
+                .news-body-table-body-title 暂无信息
+      .news-body-page
+        Page(
+          :total="pageSum",
+          :current="currentPage",
+          @on-change="onChangePage"
+        )
+</template>
+
+<script>
+
+import { getTime, getStore } from '@/util'
+import { apiArticleList } from '@/api'
+export default {
+  data () {
+    return {
+      list: [],
+      sumNum: 0,
+      pageSum: 0,
+      nodeType: 1,
+      pageIndex: 0,
+      currentPage: 1,
+      pageNumber: 10
+    }
+  },
+  mounted () {
+    this.fetchData()
+    this.onFetchSumPage()
+  },
+  methods: {
+    getTime,
+    toggleNode (x) {
+      this.nodeType = x
+      this.pageIndex = 0
+      this.onFetchSumPage(x)
+      this.fetchData({
+        nodeType: x
+      })
+    },
+    fetchData (obj = {}) {
+      const { pageSum, nodeType, pageIndex = 1 } = obj
+      this.pageIndex = pageIndex - 1
+      const language = {
+        'zh': '1',
+        'en': '2'
+      }
+      apiArticleList({
+        'theme': nodeType || 1,
+        'pageIndex': (pageIndex - 1) * this.pageNumber,
+        'pageNumber': pageSum || this.pageNumber,
+        'language': language[ getStore('lang') ] || '2'
+      }).then(res => {
+        if (pageSum > 10) {
+          this.pageSum = res.data.data.length
+        } else {
+          this.list = res.data.data
+        }
+      })
+    },
+    onChangePage (x) {
+      this.fetchData({
+        pageIndex: x,
+        nodeType: this.nodeType
+      })
+    },
+    onFetchSumPage (x) {
+      this.fetchData({
+        nodeType: x,
+        pageSum: 50000
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~@/assets/stylus/mixin.styl'
+@media screen and (max-width 1024px)
+  .news-body
+    width 90% !important
+  .news-body-table-body-date
+    margin 0 10px !important
+.news-body
+  display flex
+  flex-direction column
+  align-items center
+  padding-top 100px
+  padding-bottom 170px
+  position relative
+  width 62%
+  // background red
+  margin 0px auto
+.news-body-table
+  // width 900px
+  width 100%
+.news-body-table-btn
+  padding-top 60px
+  display flex
+  div
+    wh(240px, 60px)
+    background white
+    color $font-light
+    // background rgba(208,224,240,1)
+    border-radius 10px 10px 0px 0px
+    text-align center
+    line-height 60px
+    color #456c99
+    font-weight bold
+    border 1px solid #DAE2EB
+    box-sizing border-box
+    border-bottom 0
+.news-body-table-btn-avtive
+  color $font-dark !important
+  border 0 !important
+  background $bg-pearlblue !important
+  border none !important
+
+.news-body-table-body
+  ul
+    width 100%
+    // flex-wrap wrap
+    li
+      font-size 16px
+      width 100%
+      height 60px
+      display flex
+      align-items center
+      div
+        color #456C99
+        font-weight bold
+      .news-body-table-body-date
+        color #456C99
+        margin 0 30px 0 20px
+        white-space nowrap
+        font-size 14px
+    li:nth-child(odd)
+      background $bg-pearlblue
+    li:nth-child(even)
+      background #FBFCFE
+.news-body-page
+  position absolute
+  right 0px
+  bottom 100px
+.news-body-table-body-title
+  width 82%
+  color $font-dark !important
+</style>
